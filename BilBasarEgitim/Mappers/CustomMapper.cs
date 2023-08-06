cc4218fde161c96dc22416e67ad295af8d2bd2a4
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-08-06 20:56:26 +0300

    Create CustomMapper from JobAppealAddDto to JopAppeal

---
+++ BilBasarEgitim/Mappers/CustomMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BilBasarEgitim.Models.Dtos;
+using BilBasarEgitim.Models.Entities;
+
+namespace BilBasarEgitim.Mappers
+{
+    public class CustomMapper
+    {
+        JobAppeal JobAppealAddDtoTo(JopAppealAddDto dto)
+        {
+            return new JobAppeal()
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                BirthDate = dto.BirthDate,
+                JobRole = dto.JobRole,
+                Gender = dto.Gender,
+                MartialStatus = dto.MartialStatus,
+                Nationality = dto.Nationality,
+                GraduateSchool = dto.GraduateSchool,
+                JobExperience = dto.JobExperience
+            };
+        }
+    }
+}
