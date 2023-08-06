54790edee4fe4bbc2cad5746ea28d481e0dbf010
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-08-06 20:52:36 +0300

    Create JobAppealService

---
+++ BilBasarEgitim/Models/Dtos/JopAppealAddDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BilBasarEgitim.Models.Dtos
+{
+    public class JopAppealAddDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string BirthDate { get; set; }
+        public string JobRole { get; set; }
+        public string Gender { get; set; }
+        public string MartialStatus { get; set; }
+        public string Nationality { get; set; }
+        public string GraduateSchool { get; set; }
+        public string JobExperience { get; set; }
+    }
+}
---
+++ BilBasarEgitim/Services/JobAppealService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BilBasarEgitim.Models.Dtos;
+using BilBasarEgitim.Repositories.JobAppealRepository;
+
+namespace BilBasarEgitim.Services
+{
+    public class JobAppealService
+    {
+        private readonly JobAppealRepository _jobAppealRepository = new JobAppealRepository();
+
+        public string AddJobAppealWithCv(JopAppealAddDto dto,HttpPostedFileBase cv)
+        {
+            return "";
+        }
+    }
+}
